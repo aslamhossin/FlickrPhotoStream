9e668e59cede8e32d23827b468f780b4983fbc11
aslamhossin <aslam.iit.ju41@gmail.com>
2024-05-21 15:12:21 +0600

    feat: Photo details VM unit testing
    Signed-off-by: aslamhossin <aslam.iit.ju41@gmail.com>

--- app/src/test/java/me/aslamhossin/flickrgallery/ui/feature/deatils/PhotoDetailViewModelTest.kt
+++ app/src/test/java/me/aslamhossin/flickrgallery/ui/feature/deatils/PhotoDetailViewModelTest.kt
@@ -1,6 +1,11 @@
 package me.aslamhossin.flickrgallery.ui.feature.deatils
 
+import app.cash.turbine.test
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
+import me.aslamhossin.domain.repository.file.DownloadStatus
 import me.aslamhossin.domain.usecase.DownloadFileUseCase
 import me.aslamhossin.flickrgallery.coroutine.TestCoroutineRule
 import org.junit.Before
@@ -8,7 +13,9 @@ import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class PhotoDetailViewModelTest {
 
     @get:Rule
@@ -24,11 +31,73 @@ class PhotoDetailViewModelTest {
 
     @Test
     fun `savePhoto() calls downloadFileUseCase with correct parameters`() = runTest {
+        // Arrange
         val fileName = "test.jpg"
         val url = "https://example.com/image.jpg"
+        val downloadStatusFlow = flowOf(DownloadStatus.Completed("test.jpg"))
+        `when`(downloadFileUseCase.invoke(fileName, url)).thenReturn(downloadStatusFlow)
+
+        // Act
         viewModel.handleIntent(PhotoDetailIntent.SavePhoto(fileName, url))
+        advanceUntilIdle()
+
+        // Assert
         verify(downloadFileUseCase).invoke(fileName, url)
 
+    }
+
+
+    @Test
+    fun `savePhoto() emits completed download status`() = runTest {
+        // Arrange
+        val fileName = "test.jpg"
+        val url = "https://example.com/image.jpg"
+        val downloadStatusFlow = flowOf(DownloadStatus.Completed("test.jpg"))
+        `when`(downloadFileUseCase.invoke(fileName, url)).thenReturn(downloadStatusFlow)
+
+        // Act
+        viewModel.handleIntent(PhotoDetailIntent.SavePhoto(fileName, url))
+
+        // Assert
+        viewModel.effect.test {
+            val effect = awaitItem()
+            assert(effect is PhotoDetailEffect.ShowMessage)
+        }
+
+    }
+
+    @Test
+    fun `savePhoto() emits failed download status`() = runTest {
+        // Arrange
+        val fileName = "test.jpg"
+        val url = "https://example.com/image.jpg"
+
+        val downloadStatusFlow = flowOf(DownloadStatus.Failed.DownloadFailed("Download failed"))
+        `when`(downloadFileUseCase.invoke(fileName, url)).thenReturn(downloadStatusFlow)
+        // Act
+        viewModel.handleIntent(PhotoDetailIntent.SavePhoto(fileName, url))
+        // Assert
+        viewModel.effect.test {
+            val effect = awaitItem()
+            assert(effect is PhotoDetailEffect.ShowMessage)
+        }
+    }
+
+    @Test
+    fun `savePhoto() emits fileAlreadyExists download status`() = runTest {
+        // Arrange
+        val fileName = "test.jpg"
+        val url = "https://example.com/image.jpg"
+        val downloadStatusFlow = flowOf(DownloadStatus.Failed.FileAlreadyExists)
+        `when`(downloadFileUseCase.invoke(fileName, url)).thenReturn(downloadStatusFlow)
+
+        // Act
+        viewModel.handleIntent(PhotoDetailIntent.SavePhoto(fileName, url))
+        // Assert
+        viewModel.effect.test {
+            val effect = awaitItem()
+            assert(effect is PhotoDetailEffect.ShowMessage)
+        }
     }
 
 }
