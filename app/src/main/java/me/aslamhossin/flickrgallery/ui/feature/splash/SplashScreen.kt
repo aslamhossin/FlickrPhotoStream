671c8ac636df1e5b8b1f1407606886a6144a36a1
aminaome <aminul15-5281@diu.edu.bd>
2024-06-07 23:25:38 +0600

    ui[splash] -> Navigated to Home Screen after time out (2s)

--- app/src/main/java/me/aslamhossin/flickrgallery/ui/feature/splash/SplashScreen.kt
+++ app/src/main/java/me/aslamhossin/flickrgallery/ui/feature/splash/SplashScreen.kt
@@ -6,6 +6,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import me.aslamhossin.flickrgallery.ui.feature.splash.component.LottieBackground
+import me.aslamhossin.flickrgallery.ui.navigation.SCREEN_PHOTOS
 
 @Composable
 fun SplashScreen(
@@ -15,7 +16,7 @@ fun SplashScreen(
     val state by viewModel.state.collectAsStateWithLifecycle()
 
     if (state.isSplashTimeout) {
-
+        navHostController.navigate(SCREEN_PHOTOS)
     } else {
         LottieBackground()
     }
